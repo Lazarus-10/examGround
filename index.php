78f5cecf254a0b1361f26975a372b29fa5d628dd
Mohammad Saif Khan <ksarim225@gmail.com>
2021-11-11 02:18:53 +0530

    improved sign in modal

--- index.php
+++ index.php
@@ -7,13 +7,13 @@
 
   <title>Exam Ground</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <script src="https://use.fontawesome.com/dd653cca0e.js"></script>
   <link rel="stylesheet" href="css/main.css">
   <link rel="stylesheet" href="css/font.css">
-
-
+  
+  
   <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
   <link href='http://fonts.googleapis.com/css?family=Roboto:400,700,300' rel='stylesheet' type='text/css'>
-  <script src="https://use.fontawesome.com/dd653cca0e.js"></script>
   <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> -->
 
   <?php if (@$_GET['w']) {
@@ -71,7 +71,7 @@
 
   <!-- Sign Up form -->
   <div id="mybg-div" class = "bodyClass"  style="overflow:hidden;">
-    <div class="col-md-4 panel ">
+    <div class="col-md-4 panel panel2">
       <form class="form-horizontal" style="font-size: 0;" name="form" action="sign.php?q=account.php" onSubmit="return validateForm()" method="POST">
         <fieldset>
           <!-- Text input(name)-->
@@ -175,7 +175,8 @@
           <fieldset>
             <!-- Text input -->
             <div class="row mb-2">
-              <div class="col-sm-10 col-md-6">
+              <div class="testClass col-sm-10 col-md-6">
+                <i class = "fa fa-user fa-border"></i>
                 <input id="logIN" name="email" maxlength="50" placeholder="example@students.iiests.ac.in" class="form-control input" type="email" autofocus>
               </div>
             </div>
@@ -183,7 +184,8 @@
             <!-- Password input-->
             <div class="row mb-2">
               <!-- <label for="password" class="col-sm-2 col-form-label"></label> -->
-              <div class="col-sm-10 col-md-6">
+              <div class="testClass col-sm-10 col-md-6">
+                <i class = "fa fa-key fa-border"></i>
                 <input name="password" maxlength="15" placeholder="Enter your Password" type="password" class="form-control input">
               </div>
             </div>

0e4ae44ca21c8fa8f2886511dddd8ef07be5a027
Mohammad Saif Khan <ksarim225@gmail.com>
2021-11-12 00:13:42 +0530

    added re-verification feature

--- sign.php
+++ sign.php
@@ -6,7 +6,7 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
-function sendMail($email, $v_code){
+function sendMail($email, $v_code, $name){
     require("PHPMailer/PHPMailer.php");
     require("PHPMailer/SMTP.php");
     require("PHPMailer/Exception.php");
@@ -33,8 +33,10 @@ function sendMail($email, $v_code){
         $mail->Subject = 'E-Mail Verification from Exam Ground';
         $mail->Body    = '<div style="text-align: center; position: absolute; top:50%; left:50%; transform: translate(-50%, -50%);">'
         . '<h1 style="font-style:italic; text-align: center; color: rgb(183, 0, 255);">Exam Ground</h1>'
-        . "<p>This is a verification mail from Exam Ground. Kindly ignore this, if you haven't initiated this Mail.</p>
-        <h4>Click the Button Below to verify your account</h4>"
+        . '<h3 style="font-style:italic; text-align: center; color: blue;">Hello, '.$name.' </h3>'
+        . "<p>This is a verification mail from Exam Ground. </p> "
+        . '<p style="font-weight:bold;">Kindly Ignore this, if you haven\'t initiated this Mail.</p>'
+        . "<h3>Click the Button Below to verify your account</h3>"
         .'<span
             style="display:inline-block; font-size: 2.5rem; border: 3px solid green; border-radius: 20px; padding: 8px; background-color: black; margin: 30px;">
             <a href="https://exam-ground.herokuapp.com/verify.php?email='.$email.'&v_code='.$v_code.'" style="color: #fff; text-decoration: none;">Verify</a>
@@ -77,20 +79,52 @@ $password = md5($password);
 
 $v_code = bin2hex(random_bytes(16)); 
 
-$query =  "INSERT INTO students VALUES  ('$name', '$roll' , '$gender', '$email' ,'$mob', '$password', '$v_code', '0')";
-if (mysqli_query($con, $query) && sendMail($email, $v_code)) {
-    echo"
-    <script>
-        alert('A verification Mail has been sent to $email');
-        window.location.href='index.php';
-    </script>
-    ";
+$q = mysqli_query($con,"SELECT name, is_verified FROM students WHERE email = '$email'");
+$num_rows = mysqli_num_rows($q);
+if($num_rows == 0){
+    $query =  "INSERT INTO students VALUES  ('$name', '$roll' , '$gender', '$email' ,'$mob', '$password', '$v_code', '0')";
+    if (mysqli_query($con, $query) && sendMail($email, $v_code, $name)) {
+        echo"
+        <script>
+            alert('A verification Mail has been sent to $email');
+            window.location.href='index.php';
+        </script>
+        ";
+    }else{
+        echo"
+        <script>
+            alert('Server Down, Please try again later.');
+            window.location.href='index.php';
+        </script>
+        ";
+    }
 }else{
-    echo"
-    <script>
-        alert('A student with this E-Mail is already Registered.');
-        window.location.href='index.php';
-    </script>
-    ";
+    $result = mysqli_fetch_array($q);
+    if($result['is_verified'] == 0){
+        $query = "UPDATE `students` SET `name`='$name',`roll`='$roll',`gender`='$gender',`mob`='$mob',`password`='$password',`verification_code`='$v_code',`is_verified`='0' WHERE email = '$email'";
+        if (mysqli_query($con, $query) && sendMail($email, $v_code, $name)) {
+            echo"
+            <script>
+                alert('Re-Verification Mail has been sent to $email');
+                window.location.href='index.php';
+            </script>
+            ";
+        }else{
+            echo"
+            <script>
+                alert('Server Down, Please try again later.');
+                window.location.href='index.php';
+            </script>
+            ";
+        }
+    }else{
+        echo"
+        <script>
+            alert('A student with this E-Mail is already Registered.');
+            window.location.href='index.php';
+        </script>
+        ";
+    }
 }
+
 ob_end_flush();
